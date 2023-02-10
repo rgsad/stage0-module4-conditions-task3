ac31526b5e719e2032f03a0787684fa331212151
rgsad <rsad3262@gmail.com>
2023-02-11 00:17:23 +0300

    update refactor task

--- src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
+++ src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
@@ -2,16 +2,16 @@ package school.mjc.stage0.conditions.task3;
 
 public class Seasons {
     public void tellTheSeasonByMonthNumber(int month) {
-        if (month == 12 || (month > 0 && month < 3)) {
-            System.out.println("Winter");
-        } else if (month >= 3 && month < 6) {
+       if (month < 1 || month > 12) {
+           System.out.println("wrong number!");
+       } else if (month >= 3 && month < 6) {
             System.out.println("Spring");
         } else if (month >= 6 && month < 9) {
             System.out.println("Summer");
-        } else if (month < 12) {
+        } else if (month >= 9 && month < 12) {
             System.out.println("Autumn");
         } else {
-            System.out.println("wrong number!");
-        }
+           System.out.println("Winter");
+       }
     }
 }
